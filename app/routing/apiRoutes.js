14d4bdba556de5efabe767879f6c12d999bd6690
Lysia Stolhofer <lysiapaulinstolhofer@gmail.com>
2019-11-05 23:36:06 -0500

    problems solved with server connection

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,6 +1,6 @@
 
 // LOAD DATA
-// These data sources hold arrays of information on all possible friends
+// data sources hold arrays of information on all possible friends
 var friends = require('../data/friends.js');
 
 // ROUTING
@@ -22,26 +22,22 @@ module.exports = function(app){
 			friendDifference: 1000
 		};
 
-		// Here we take the result of the user's survey POST and parse it.
 		var userData 	= req.body;
 		var userName 	= userData.name;
 		var userPhoto 	= userData.photo;
 		var userScores 	= userData.scores;
-
-		// This variable will calculate the difference between the user's scores and the scores of
-		// each user in the database
 		var totalDifference = 0;
 
-		// Here we loop through all the friend possibilities in the database. 
+		// loop through all the friend possibilities in the database. 
 		for  (var i=0; i< friends.length; i++) {
 
 			console.log(friends[i].name);
 			totalDifference = 0;
 
-			// We then loop through all the scores of each friend
+			// loop through all the scores of each friend
 			for (var j=0; j< friends[i].scores[j]; j++){
 
-				// We calculate the difference between the scores and sum them into the totalDifference
+				// calculate the difference between the scores and sum them into the totalDifference
 				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friends[i].scores[j]));
 
 				// If the sum of differences is less then the differences of the current "best match"
@@ -55,11 +51,8 @@ module.exports = function(app){
 			}
 		}
 
-		// Finally save the user's data to the database (this has to happen AFTER the check. otherwise,
-		// the database will always return that the user is the user's best friend).
 		friends.push(userData);
 
-		// Return a JSON with the user's bestMatch. This will be used by the HTML in the next page. 
 		res.json(bestMatch);
 
 	});
--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(express.static(path.join(__dirname, './app/public/assets')))
 
 require('./app/routing/apiRoutes.js')(app);
 require('./app/routing/htmlRoutes.js')(app);
